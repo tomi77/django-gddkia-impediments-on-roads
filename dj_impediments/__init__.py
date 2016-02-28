3b70c20a987131ce784b5d2abdbe4559c0820daa
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-02-28 20:08:06 +0100

    change version

--- dj_impediments/__init__.py
+++ dj_impediments/__init__.py
@@ -2,7 +2,7 @@ from django import get_version
 
 from .cache import get_impediments
 
-VERSION = (0, 1, 0, 'beta', 1)
+VERSION = (0, 1, 0, 'final', 0)
 __author__ = "Tomasz Jakub Rup"
 __email__ = "tomasz.rup@gmail.com"
 __version__ = get_version(VERSION)
