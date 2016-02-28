289124ada42c67be7ae19557f171d49f71dc1391
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-02-28 18:57:31 +0100

    fix python2.7 / django 1.4 test

--- test_settings.py
+++ test_settings.py
@@ -1,1 +1,12 @@
 SECRET_KEY = 'qaz123'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
