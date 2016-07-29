2a0e2de6f147b527aa781819c8dd7d2bebc43e35
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-07-29 12:56:27 +0200

    fix first installation

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from codecs import open
 
 from setuptools import setup, find_packages
 
-from dj_impediments import __version__, __author__, __email__, __license__
-
 setup(
     name="django-gddkia-impediments-on-roads",
-    version=__version__,
-    author=__author__,
-    author_email=__email__,
+    version='0.1.1',
+    author='Tomasz Jakub Rup',
+    author_email='tomasz.rup@gmail.com',
     url='https://github.com/tomi77/django-gddkia-impediments-on-roads',
     description='Django client for gddkia-impediments-on-roads',
     long_description=open("README.rst").read(),
@@ -35,7 +33,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities',
     ],
-    license=__license__,
+    license='MIT',
     packages=find_packages(),
     install_requires=[
         'django>=1.4',
